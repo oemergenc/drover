acfd88985b5b8f0388ad08bc007b8521478a4e54
Oemer Genc <oemergenc@gmail.com>
2019-04-01 20:07:23 +0200

    Some more shizzle

--- src/drover_arduino.ino
+++ src/drover_arduino.ino
@@ -14,12 +14,12 @@
 //AF_DCMotor motor2(2);
 //AF_DCMotor motor3(3);
 //AF_DCMotor motor4(4);
-
+//#define __AVR_ATmega2560__ 1
 
 MS_DCMotor motor1(1);
 MS_DCMotor motor2(2);
-MS_DCMotor motor3(3);
-MS_DCMotor motor4(4);
+//MS_DCMotor motor3(3);
+//MS_DCMotor motor4(4);
 
 const float stepcount = 10.00;  // 10 Slots in disk, change if different
 volatile int counter_left = 0;
@@ -40,54 +40,58 @@ void docount_right()  // counts from the speed sensor
 {
     counter_right++;  // increase +1 the counter value
 }
-
-void timerIsr()
-{
-    Timer1.detachInterrupt();  //stop the timer
-    rpmLeft = (counter_left / stepcount) * 60.00;  // calculate RPM for Motor 1
-    rpmRight = (counter_right / stepcount) * 60.00;  // calculate RPM for Motor 2
-    counter_right = 0;
-    counter_left = 0;
-    Timer1.attachInterrupt(timerIsr);  //enable the timer
-}
+//
+//void timerIsr()
+//{
+//    Timer1.detachInterrupt();  //stop the timer
+//    rpmLeft = (counter_left / stepcount) * 60.00;  // calculate RPM for Motor 1
+//    rpmRight = (counter_right / stepcount) * 60.00;  // calculate RPM for Motor 2
+//    counter_right = 0;
+//    counter_left = 0;
+//    Timer1.attachInterrupt(timerIsr);  //enable the timer
+//}
 
 void setSpeed(int speed)
 {
+    motor1.run(BACKWARD);
     motor1.setSpeed(speed);
+    motor2.run(BACKWARD);
     motor2.setSpeed(speed);
-    motor3.setSpeed(speed);
-    motor4.setSpeed(speed);
+//    motor3.setSpeed(speed);
+//    motor4.setSpeed(speed);
 }
 
 void setup()
 {
 //    TCCR5B = (TCCR5B & 0xF8) | 0x05 ;
     Serial.begin(9600);
+     // and change this function to .run(FORWARD)
+
     //Setup for encoders
     pinMode(right_encoder_pin, INPUT_PULLUP);
     pinMode(left_encoder_pin, INPUT_PULLUP);
 
-    Timer1.initialize(LOOP_TIME);
+//    Timer1.initialize(LOOP_TIME);
     attachInterrupt(digitalPinToInterrupt(left_encoder_pin), docount_left,
                     CHANGE);  // increase counter when speed sensor pin goes High
     attachInterrupt(digitalPinToInterrupt(right_encoder_pin), docount_right,
                     CHANGE);  // increase counter when speed sensor pin goes High
-    Timer1.attachInterrupt(timerIsr); // enable the timer
+//    Timer1.attachInterrupt(timerIsr); // enable the timer
 }
 
 void loop()
 {
-    motor1.run(BACKWARD); // and change this function to .run(FORWARD)
-    motor2.run(FORWARD); // and change this function to .run(FORWARD)
-    motor3.run(FORWARD); // and change this function to .run(FORWARD)
-    motor4.run(BACKWARD); // and change this function to .run(FORWARD)
-    if (millis() - start >= 1000)
+//    motor1.run(BACKWARD); // and change this function to .run(FORWARD)
+//    motor2.run(BACKWARD); // and change this function to .run(FORWARD)
+//    motor3.run(FORWARD); // and change this function to .run(FORWARD)
+//    motor4.run(BACKWARD); // and change this function to .run(FORWARD)
+    if (millis() - start >= 500)
     {
 
-//        rpmLeft = (counter_left / stepcount) * 60;  // calculate RPM for Motor 1
-//        rpmRight = (counter_right / stepcount) * 60;  // calculate RPM for Motor 2
-//        Serial.println("left");
-//        Serial.println("right");
+        rpmLeft = (counter_left / stepcount) * 60;  // calculate RPM for Motor 1
+        rpmRight = (counter_right / stepcount) * 60;  // calculate RPM for Motor 2
+        Serial.println("left");
+        Serial.println("right");
         Serial.print("Motor Speed 1: ");
         Serial.print(counter_left);
         Serial.print(" Counts, ");
@@ -100,8 +104,8 @@ void loop()
         Serial.print(" RPM, ");
         Serial.print(speed);
         Serial.println(" Speed");
-//        counter_right = 0;
-//        counter_left = 0;
+        counter_right = 0;
+        counter_left = 0;
         start = millis();
     }
     if (Serial.available())
@@ -131,9 +135,9 @@ void loop()
         else if (command == 'm')
         {
             speed = speed - 10;
-            if (speed < 0)
+            if (speed < -255)
             {
-                speed = 0;
+                speed = -255;
             }
             setSpeed(speed);
         }
